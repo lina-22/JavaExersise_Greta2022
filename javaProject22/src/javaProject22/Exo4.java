61c62015dc9e367aa56063b1bed3c7cc21311419
lina-22 <haquefahinur@gmail.com>
2022-10-26 14:44:32 +0200

    Bismillahir Rahmanir Rahim

---
+++ javaProject22/src/javaProject22/Exo4.java
@@ -0,0 +1,7 @@
+package javaProject22;
+
+public class Exo4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ javaProject22/src/javaProject22/Exo5.java
@@ -0,0 +1,29 @@
+package javaProject22;
+
+import java.util.Scanner;
+
+public class Exo5 {
+
+    public static void main(String[] args) {
+        int[] annee = {31,28,31,31,30,31,30,31,31,30,31,30,31};
+        Scanner sc = new Scanner(System.in);
+        int mois, jour, nb=0;
+        do {
+            System.out.println("Quel est la mois:");
+            mois= sc.nextInt();
+        }while(mois<=0||mois>12);
+        do{
+            System.out.println("Quel est le jour? ");
+            jour= sc.nextInt();
+        }while(jour<=0 || jour>annee[mois-1]);
+
+        int i = 0;
+        while (i<mois-1){
+            nb = nb+annee[i];
+            i++;
+        }
+        nb = nb+jour;
+        System.out.println("Nombre de jours demandé: "+nb);
+
+    }
+}
